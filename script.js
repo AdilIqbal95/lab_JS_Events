24123997c1309afbf29bfbef4765a3dbd95967a0
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-06-24 17:30:48 +0100

    attempted extensions

--- script.js
+++ script.js
@@ -3,24 +3,53 @@ const formData = document.querySelector("#new-todo");
 const list = document.querySelector("#list");
 const showDate = document.querySelector("#show-date-btn");
 const dateDiv = document.querySelector("#date-div");
-
-// console.log(toDoForm);
+const completedList = document.querySelector("#completed")
 
 toDoForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(event);
     const newListItem = document.createElement("li");
+    
     const newButton = document.createElement("button");
     newButton.setAttribute("Class","Delete-button");
     newButton.innerText = "Delete";
+
+    const completeButton = document.createElement("button");
+    completeButton.setAttribute("Class","Complete-button");
+    completeButton.innerText = "Completed";
+    
     newListItem.innerText = event.target["new-todo"].value;
     list.appendChild(newListItem);
     newListItem.appendChild(newButton);
-    newButton.addEventListener("click", (event) =>{
-        
+    newListItem.appendChild(completeButton);
+    console.log(event);
+    newButton.addEventListener("click", () =>{
+        removeElement(newListItem);
     })
+    console.log(event.node)
+    const completedListItem = document.createElement("li");
+    completeButton.addEventListener("click", () => {
+        // Change colour to grey
+
+        // Move item to new list
+        completedListItem.innerText = event.target["new-todo"].value
+        completedList.appendChild(completedListItem);
+
+    })
+
+
 })
 
+const deleteButtonFunction = (newButton) => {
+
+}
+
+const removeElement = (element) => {
+    element.remove()
+}
+
+const markAsComplete = () => {
+
+}
 
 showDate.addEventListener("click", (event) => {
     const newParagraph = document.createElement("p");
@@ -29,6 +58,8 @@ showDate.addEventListener("click", (event) => {
     dateDiv.appendChild(newParagraph);
     console.log(newParagraph);
 })
+
+
 
 
 
